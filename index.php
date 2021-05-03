5d41b5906c03adb8c08365649e31f58dcd770af9
Sergio <sergio.marretta@libero.it>
2021-05-03 17:08:47 +0200

    part1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$badword1 = $_GET["badword1"];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, <?php echo $badword1 ?> consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, fuck quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate fuck velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint fuck occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim fuck id est laborum.</p>
+  </body>
+</html>
